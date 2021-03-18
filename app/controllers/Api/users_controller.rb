111fa15619470572cdf11db1175509ea39069bde
Liubov Tarasenko <liubov.tarasenko@masterofcode.com>
2021-03-18 20:18:56 +0200

    update profile info

--- app/controllers/Api/users_controller.rb
+++ app/controllers/Api/users_controller.rb
@@ -3,12 +3,21 @@ class Api::UsersController < ActionController::API
   def update
     user_params = JSON.parse(params[:user])
     name = "#{user_params['name']} #{user_params['soname']}"
-    avatar = params['avatar']
-    uploaded_avatar = Cloudinary::Uploader.upload(avatar.path) if params[:avatar]
-    current_user.update(name: name, avatar: uploaded_avatar['url'])
+    current_user.update(name: name)
+
+    if params['avatar'] && params['avatar'] != 'undefined'
+      uploaded_avatar = Cloudinary::Uploader.upload(params['avatar'].path)
+      current_user.update(avatar: uploaded_avatar['url'])
+    end 
+    
+    render_user
   end
 
   def show
+    render_user
+  end
+
+  def render_user
     user = {
       name: current_user.name,
       avatar: current_user.avatar
