16005f581b2f77c0b9c7e02b3ce230fdeafb52e0
luba <lubo4ka.boiko@gmail.com>
2021-04-09 23:09:06 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,16 @@
 class UserMailer < ApplicationMailer
+  def proposal_email
+    @from_user = params[:from_user]
+    @product_to_give = params[:product_to_give]
+    @to_user = params[:to_user]
+    @product_to_get = params[:product_to_get]
+
+    mail(to: @to_user.email, subject: proposal_subject)
+  end
+
+  private
+
+  def proposal_subject
+    @product_to_get.kind == "обміняю" ? "Нова пропозиція обмінятись" : "Нова ропозиція забрати"
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
   config.action_mailer.delivery_method = :sendmail
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -47,7 +46,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'example.com',
-    user_name: 'shop67947@gmail.com',
+    user_name: 'toyshare2103@gmail.com',
     password: ENV['MAILER_SECRET'],
     authentication: 'plain',
     enable_starttls_auto: true
@@ -59,7 +58,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation = :raise
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   # Tell Active Support which deprecation messages to disallow.
