e3257e48285630b9602fba39c86fea750761a492
luba <lubo4ka.boiko@gmail.com>
2021-05-14 20:12:20 +0300

    active admin configs

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :kind, :category_id, :subcategory_id, :user_id, :status, :url_name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :kind, :category_id, :subcategory_id, :user_id, :status, :url_name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
